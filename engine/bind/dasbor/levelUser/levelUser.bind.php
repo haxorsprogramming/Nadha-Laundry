5592dfdc83e306ce1fa9907c6b5b2add11719456
Haxorsprogramming <haxorsuinsu@gmail.com>
2020-05-05 17:43:27 +0700

    hotdog

--- engine/bind/dasbor/levelUser/levelUser.bind.php
+++ engine/bind/dasbor/levelUser/levelUser.bind.php
@@ -1,10 +1,7 @@
-<div class="container">
+<div class="row">
   <div style='margin-bottom:15px;' id='divOperasi'>
   <a href='#!' class='btn btn-lg btn-primary  btn-icon icon-left' v-on:click='tambahPelanggan'><i class="fas fa-plus-circle"></i> Tambah Level User</a>
   </div>
-  <div class="row" id='divLevelUser'>
-  <table id='tblLevelUser' class='table'>
-  </table>
-</div>
+
 </div>
 <script src="<?=STYLEBASE; ?>/dasbor/levelUser.js"></script>
