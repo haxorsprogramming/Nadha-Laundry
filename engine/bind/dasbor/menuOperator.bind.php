81edc634c297fb701f05b26472210cb091541c31
Haxorsprogramming <haxorsuinsu@gmail.com>
2020-05-19 21:27:06 +0700

    aku seorang boyband

--- engine/bind/dasbor/menuOperator.bind.php
+++ engine/bind/dasbor/menuOperator.bind.php
@@ -1,17 +1,17 @@
 <ul class="sidebar-menu" style='margin-top:20px;'>
   <!-- <li class="menu-header">Menu</li> -->
-  <li><a class="nav-link btnDashboard" href="#!" v-on:click='berandaAct'><i class="fas fa-newspaper"></i> <span>Dashboard</span></a></li>
-  <li><a class="nav-link btnKartuLaundry" href="#!" v-on:click='kartuLaundryAtc'><i class="fas fa-newspaper"></i> <span>Kartu Laundry</span></a></li>
-  <li><a class="nav-link btnLaundryRoom" href="#!" v-on:click='laundryRoomAtc'><i class="fas fa-newspaper"></i> <span>Laundry Room</span></a></li>
+  <li><a class="nav-link btnDashboard" href="#!" v-on:click='berandaAct'><i class="fas fa-home"></i> <span>Dashboard</span></a></li>
+  <li><a class="nav-link btnKartuLaundry" href="#!" v-on:click='kartuLaundryAtc'><i class="fas fa-clipboard"></i> <span>Kartu Laundry</span></a></li>
+  <li><a class="nav-link btnLaundryRoom" href="#!" v-on:click='laundryRoomAtc'><i class="fas fa-tshirt"></i> <span>Laundry Room</span></a></li>
   <li class="dropdown">
     <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-newspaper"></i> <span>Data Master</span></a>
     <ul class="dropdown-menu">
       <li><a class="nav-link" href="#!" v-on:click='pelangganAtc'>Pelanggan</a></li>
     </ul>
   </li>
-  <li><a class="nav-link " href="#!" v-on:click='dataTransaksiAtc'><i class="fas fa-newspaper"></i> <span>Data Transaksi</span></a></li>
+  <li><a class="nav-link " href="#!" v-on:click='dataTransaksiAtc'><i class="fas fa-receipt"></i> <span>Data Transaksi</span></a></li>
   <li class="dropdown">
-    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-newspaper"></i> <span>Laporan & Statistik</span></a>
+    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-chart-line"></i> <span>Laporan & Statistik</span></a>
     <ul class="dropdown-menu">
       <li><a class="nav-link" href="#!" v-on:click='arusKasAtc'>Arus Kas</a></li>    
     </ul>
--- engine/route/laundryRoom.route.php
+++ engine/route/laundryRoom.route.php
@@ -3,33 +3,34 @@
 class laundryRoom extends Route{
 
    public function __construct()
-   {
-   $this -> st = new state;
-   }
+    {
+    $this -> st = new state;
+    }
 
    public function index()
-   {
-    $this -> cekUserLogin('userSes');
-    $this -> st -> query("SELECT * FROM tbl_laundry_room WHERE status !='finish' ORDER BY id DESC;");
-    $data['laundryRoom'] = $this -> st -> queryAll();
-    $this -> bind('dasbor/laundryRoom/laundryRoom', $data);
-   }
+    {
+        $this -> cekUserLogin('userSes');
+        $this -> st -> query("SELECT * FROM tbl_laundry_room WHERE status !='finish' ORDER BY id DESC;");
+        $data['laundryRoom'] = $this -> st -> queryAll();
+        $this -> bind('dasbor/laundryRoom/laundryRoom', $data);
+    }
 
    public function detailCucian()
-   {
-       $this -> cekUserLogin('userSes');
-       $this -> st -> query("SELECT * FROM tbl_service WHERE aktif='y';");
-       $data['listProduk'] = $this -> st -> queryAll();
-       $data['kd'] = $this -> inp('kd');
-       $this -> bind('dasbor/laundryRoom/detailCucian', $data);
-   }
+    {
+        $this -> cekUserLogin('userSes');
+        $this -> st -> query("SELECT * FROM tbl_service WHERE aktif='y';");
+        $data['listProduk'] = $this -> st -> queryAll();
+        $data['kd'] = $this -> inp('kd');
+        $this -> bind('dasbor/laundryRoom/detailCucian', $data);
+    }
 
-   public function getInfoProduk(){
-       $kdProduk = $this -> inp('kdProduk');
-       $this -> st -> query("SELECT * FROM tbl_service WHERE kd_service='$kdProduk';");
-       $dProduk = $this -> st -> querySingle();
-       $this -> toJson($dProduk);
-   }
+   public function getInfoProduk()
+    {
+        $kdProduk = $this -> inp('kdProduk');
+        $this -> st -> query("SELECT * FROM tbl_service WHERE kd_service='$kdProduk';");
+        $dProduk = $this -> st -> querySingle();
+        $this -> toJson($dProduk);
+    }
 
    public function prosesTambahItem()
    {
