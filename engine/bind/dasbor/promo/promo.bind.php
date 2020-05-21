30ffd0110be5c7bab382cc9294bac8af43db7744
Haxorsprogramming <haxorsuinsu@gmail.com>
2020-05-22 00:33:32 +0700

    bismillah

--- engine/bind/dasbor/promo/promo.bind.php
+++ engine/bind/dasbor/promo/promo.bind.php
@@ -2,10 +2,10 @@
     <a href='#!' class='btn btn-lg btn-primary  btn-icon icon-left'><i class="fas fa-plus-circle"></i> Tambah Promo</a>
 </div>
 <div class="col-lg-6 col-md-6 col-sm-6 col-12"  id='divPromo'>
-    <table id='tblPromo' class="table">
+    <table id='tblPromo' class="table table-hover">
         <thead>
             <tr>
-            <td>Kd Promo</td><td>Deks</td><td>Digunakan</td><td>Status</td>
+            <td>Kd Promo</td><td>Deks</td><td>Diskon</td><td>Status</td><td></td>
             </tr>
         </thead>
         <tbody>
@@ -14,10 +14,12 @@
                 <td>{{dp.deks}}</td>
                 <td>{{dp.diskon}}</td>
                 <td></td>
+                <td></td>
             </tr>
         </tbody>
     </table>
 </div>
 <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+
 </div>
 <script src="<?=STYLEBASE; ?>/dasbor/promo.js"></script>
