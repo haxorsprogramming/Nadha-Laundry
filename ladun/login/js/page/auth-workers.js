c47f96f86a5692c6ecb496c357dc4d24d51331e3
Haxorsprogramming <haxorsuinsu@gmail.com>
2020-05-25 01:58:02 +0700

    mudahmudahan;

--- ladun/login/js/page/auth-workers.js
+++ ladun/login/js/page/auth-workers.js
@@ -1,2 +1,20 @@
-//workers for nadhamedia rules 
-//NOTE : Dont change or delete this file, beacause nadhamedia need this file to montoring client activities
+//Workers for nadhamedia 
+//NOTE : Dont change or delete this file, because nadhamedia need this file to monitoring client activity
+var workersNadha = new Vue({
+    el : '#main-section',
+    data : {
+        serverAddress : 'http://service.haxors.or.id/product/nadhaLaundry/backup',
+        serverStatus : '',
+        clientId : ''
+    },
+    methods : {
+        getInfoClient : function()
+        {
+            $.post(this.serverAddress, function(data){
+
+            });
+        }
+    }
+});
+
+workersNadha.getInfoClient();
